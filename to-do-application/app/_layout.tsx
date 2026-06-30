7da361c1b00225f0ff0f60d4b38060751771b7bf
Rawat890 <rohanrawat7508@gmail.com>
2026-06-30 11:41:57 +0530

    feat: enhance RootLayout with ConvexProvider and SplashScreen handling

--- to-do-application/app/_layout.tsx
+++ to-do-application/app/_layout.tsx
@@ -1,5 +1,33 @@
+import { useAppFonts } from "@/hooks/useAppFonts";
+import { ThemeProvider } from "@/hooks/useTheme";
+import { ConvexProvider, ConvexReactClient } from 'convex/react';
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+
+const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
+  unsavedChangesWarning: false
+})
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  return <Stack />;
+  const fontsLoaded = useAppFonts();
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+
+  return (
+    <ConvexProvider client={convex}>
+      <ThemeProvider>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(tabs)" />
+        </Stack>
+      </ThemeProvider>
+    </ConvexProvider>
+  )
 }
